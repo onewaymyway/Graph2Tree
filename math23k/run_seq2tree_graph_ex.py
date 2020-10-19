beb628a26706d8c45b2086e83cb319163587744d
onewaymyway <839357821@qq.com>
2020-10-20 00:03:57 +0800

    尝试重现bug

--- math23k/run_seq2tree_graph_ex.py
+++ math23k/run_seq2tree_graph_ex.py
@@ -242,6 +242,8 @@ print("build test_pairs")
 test_pairs = model_info.build_langed_pairs(pairs_tested, "test")
 print("build pairs complete")
 
+train_pairs=train_pairs[76800:]
+
 #train_pairs=train_pairs[]
 # input_lang, output_lang, train_pairs, test_pairs = prepare_data(pairs_trained, pairs_tested, 5, generate_nums,copy_nums, tree=True)
 
